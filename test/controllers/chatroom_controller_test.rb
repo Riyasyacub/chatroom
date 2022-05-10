3bcf09421b51d3844f6afad04939e6b49965502f
riyasyacub <riyasyacub@gmail.com>
2022-05-10 09:21:14 +0530

    Add root and login routes

---
+++ test/controllers/chatroom_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class ChatroomControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get chatroom_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get chatroom_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get chatroom_new_url
+    assert_response :success
+  end
+end
